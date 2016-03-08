7710cf3bc5ec86ffcac7aa60b6a5fe87531ca5a8
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2016-03-08 13:44:04 +0200

    chore: upgrade to angular beta.8

--- test/test.ts
+++ test/test.ts
@@ -1,35 +1,39 @@
 import {
-  it,
-//  inject,
-  injectAsync,
-  describe,
-//  beforeEachProviders,
-  TestComponentBuilder
+    it,
+   // inject,
+    injectAsync,
+    describe,
+  //  beforeEachProviders,
+    TestComponentBuilder
 } from 'angular2/testing';
 
 import {Component/*, provide*/} from 'angular2/core';
-// import {BaseRequestOptions, Http} from 'angular2/http';
-// import {MockBackend} from 'angular2/http/testing';
+//import {BaseRequestOptions, Http} from 'angular2/http';
+//import {MockBackend} from 'angular2/http/testing';
 
 // Load the implementations that should be tested
 import KendoAngularComponent from '../src/kendo-component';
 
+import {setBaseTestProviders} from 'angular2/testing';
+import {TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS} from 'angular2/platform/testing/browser';
+
+setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS);
+
 describe('kendo-angular-component', () => {
-  // Create a test component to test directives
-  @Component({
-    directives: [ KendoAngularComponent ],
-    template: ''
-  })
-  class TestComponent {}
+    // Create a test component to test directives
+    @Component({
+        directives: [ KendoAngularComponent ],
+        template: ''
+    })
+    class TestComponent {}
 
-  it('should render a div', injectAsync([TestComponentBuilder], (tcb: any) => {
-
-    return tcb.overrideTemplate(TestComponent, '<kendo-angular-component></kendo-angular-component>')
-      .createAsync(TestComponent).then((fixture: any) => {
-        fixture.detectChanges();
-        let compiled: any = fixture.debugElement.nativeElement.children[0].children[0];
-        expect(compiled.tagName).toBe('DIV');
-      });
-  }));
+    it('should render a div', injectAsync([TestComponentBuilder], (tcb: any) => {
+        return tcb.overrideTemplate(TestComponent, '<kendo-angular-component></kendo-angular-component>')
+            .createAsync(TestComponent).then((fixture: any) => {
+                fixture.detectChanges();
+                let compiled: any = fixture.debugElement.nativeElement.children[0].children[0];
+                expect(compiled.tagName).toBe('DIV');
+            });
+    }));
 
 });
